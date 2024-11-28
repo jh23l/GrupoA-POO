90cdd967f00ca4e159d3d8e2e0a553e5cb0999d5
Karolyn Paulino <138624144+LynPau27@users.noreply.github.com>
2024-11-28 14:35:15 -0400

    Creación de la interfaz Operaciones y la clase abstracta

---
+++ GrupoA-POO/CalculadoraBase/CalculadoraBase.cs
@@ -0,0 +1,55 @@
+﻿using GrupoA_POO.Operaciones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrupoA_POO.CalculadoraBase
+{
+    abstract class CalculadoraBase : IOperaciones
+    {
+        public double Sumar(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Restar(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiplicar(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Dividir(double a, double b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("No se puede dividir entre 0.");
+                return double.NaN; 
+            }
+            else
+            {
+                return a / b;
+            }
+        }
+
+        public double CalcularAreaCirculo(double radio)
+        {
+            return radio * radio * Math.PI;
+        }
+
+        public double CalcularVolumenEsfera(double radio)
+        {
+            return (4.0 / 3.0) * Math.PI * Math.Pow(radio, 3);
+        }
+
+        public double CalcularDistancia(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+    }
+}
---
+++ GrupoA-POO/Operaciones/IOperaciones.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrupoA_POO.Operaciones
+{
+    public interface IOperaciones
+    {
+        // Suma dos números. 
+        double Sumar(double a, double b);
+        // Resta dos números. 
+        double Restar(double a, double b);
+        //Multiplica dos números. 
+        double Multiplicar(double a, double b);
+        //Divide dos números(manejar la división por cero). 
+        double Dividir(double a, double b);
+        //Calcula el área de un círculo. 
+        double CalcularAreaCirculo(double radio);
+        //Calcula el volumen de una esfera. 
+        double CalcularVolumenEsfera(double radio);
+        //Calcula la distancia entre dos puntos en un plano cartesiano.  double CalcularDistancia(double x1, double y1, double x2, double y2); } 
+
+    }
+
+
+}
